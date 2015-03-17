a644075a7e42b91d04656a139892ae586ed4b52a
Joe Looney <hello@joelooney.org>
2015-03-17 11:00:28 -0400

    Update pushtx.js

--- js/xcp-js/pushtx.js
+++ js/xcp-js/pushtx.js
@@ -3,12 +3,11 @@ function ajax(url, data) {
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function () {
         if (xhr.readyState == 4) {
+            
             console.log(xhr.responseText);
             
-            $("#sendtokenbutton").html("Sent! Refresh to continue...");
-            //$("#sendtokenbutton").prop('disabled', true);
-            
             xhr.close;
+            
         }
     }
     xhr.open(data ? "POST" : "GET", url, true);
