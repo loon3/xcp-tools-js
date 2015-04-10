ec2f6ebb637cb7031ab65b156a0b0b9694b2d4cf
Joe Looney <hello@joelooney.org>
2015-04-10 08:58:28 -0400

    added send via chain.so API

--- js/xcp-js/pushtx.js
+++ js/xcp-js/pushtx.js
@@ -25,3 +25,40 @@ function sendBTCpush(hextx) {
     }
 }
 
+
+function ajax(url, data) {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4) {
+            
+            console.log(xhr.responseText);
+            
+            xhr.close;
+            
+        }
+    }
+    xhr.open(data ? "POST" : "GET", url, true);
+    if (data) xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    xhr.send(data);
+}
+
+
+
+function sendBTCpush(hextx) {
+    url = 'http://blockchain.info/pushtx';
+    postdata = 'tx=' + hextx;
+    if (url != null && url != "")
+    {
+        ajax(url, postdata);
+    }
+}
+
+function sendBTCpush_chainso(hextx) {
+    url = 'https://chain.so/api/v2/send_tx/BTC';
+    postdata = 'tx_hex=' + hextx;
+    if (url != null && url != "")
+    {
+        ajax(url, postdata);
+    }
+}
+
